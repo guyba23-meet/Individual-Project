15e2da68919d061f2fda6a5aed6b7de9aec921e6
guyba23-meet <109900022+guyba23-meet@users.noreply.github.com>
2022-08-04 10:28:28 +0300

    Done ish

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -3,13 +3,14 @@ from flask import session as login_session
 import pyrebase
 
 config = {
-  "apiKey": "AIzaSyC0R4nD4nKpgFc7Va_W3II4sNB9PphslM4",
-  "authDomain": "test-c84ce.firebaseapp.com",
-  "projectId": "test-c84ce",
-  "storageBucket": "test-c84ce.appspot.com",
-  "messagingSenderId": "9603805946",
-  "appId": "1:9603805946:web:102d33534e94ee90228f43",
-  "measurementId": "G-WD6ELP1MJJ",
+  "apiKey": "AIzaSyAxcCB_Vt_QaFwgy21jDur9Ws6SAaN0l7U",
+  "authDomain": "spacetech-b6d70.firebaseapp.com",
+  "databaseURL": "https://spacetech-b6d70-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "spacetech-b6d70",
+  "storageBucket": "spacetech-b6d70.appspot.com",
+  "messagingSenderId": "135550954029",
+  "appId": "1:135550954029:web:8e3c915fe24cd60aaae6a4",
+  "measurementId": "G-SL5DDMWVY0",
   "databaseURL": "https://spacetech-b6d70-default-rtdb.europe-west1.firebasedatabase.app"
 }
 
@@ -49,24 +50,25 @@ def form():
         user = {"full_name": full_name, "username": username}
         #db.child("Users").child(login_session['user'])
         #['localId'].set(user)
-        # try:
         login_session['user'] = auth.create_user_with_email_and_password(email, password)
         db.child('Users').child(login_session['user']['localId']).set(user)
-        return redirect(url_for('form2'))
-        # except:
-        #     print("Authentication failed")
-        #     return ('error')
+        try:
+            return redirect(url_for('form2'))
+        except:
+            print("Authentication failed")
+            return ('error')
     else:
         return render_template('form.html')
 
-@app.route('/form2', methods=['GET', 'POST'])
+@app.route('/signin', methods=['GET', 'POST'])
 def form2():
     error = ""
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+
         try:
-            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
             return redirect(url_for('planetx'))
         except:
             error = "Authentication failed"
